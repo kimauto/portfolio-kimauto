431fc3ecfc5e8d67ac1261c6bb51cfce5e32e022
kimauto <dkskdksk111@gmail.com>
2024-08-13 16:26:49 +0900

    서비스 개발

--- src/main/kotlin/com/kimauto/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/kimauto/portfolio/presentation/service/PresentationService.kt
@@ -1,7 +1,50 @@
 package com.kimauto.portfolio.presentation.service
 
+import com.kimauto.portfolio.domain.entity.Introduction
+import com.kimauto.portfolio.presentation.dto.IntroductionDTO
+import com.kimauto.portfolio.presentation.dto.LinkDTO
+import com.kimauto.portfolio.presentation.dto.ProjectDTO
+import com.kimauto.portfolio.presentation.dto.ResumeDTO
+import com.kimauto.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import kotlin.math.exp
 
 @Service
-class PresentationService {
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+    @Transactional(readOnly = true) // 조회성 메서드 성능 이점 있음
+    fun getIntroductions():List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks():List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+
+        return links.map { LinkDTO(it) }
+    }
+
+
+    @Transactional(readOnly = true)
+    fun getResume() :ResumeDTO{
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects():List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+
+        return projects.map { ProjectDTO(it) }
+    }
 }
