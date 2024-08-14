c05a195894ed985139c69e2d8b3b4a185f796fba
kimauto <dkskdksk111@gmail.com>
2024-08-14 14:33:43 +0900

    컨트롤러 개발

--- src/main/kotlin/com/kimauto/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/kimauto/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,44 @@
 package com.kimauto.portfolio.presentation.controller
 
+import com.kimauto.portfolio.domain.entity.Introduction
+import com.kimauto.portfolio.presentation.dto.IntroductionDTO
+import com.kimauto.portfolio.presentation.dto.LinkDTO
+import com.kimauto.portfolio.presentation.dto.ProjectDTO
+import com.kimauto.portfolio.presentation.dto.ResumeDTO
+import com.kimauto.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "Hello World!"
     }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
+
 }
