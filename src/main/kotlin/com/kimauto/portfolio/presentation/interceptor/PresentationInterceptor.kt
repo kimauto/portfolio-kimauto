730ddf65028aa838aa6e95f2e3d522d7a411c7f1
kimauto <dkskdksk111@gmail.com>
2024-08-22 14:07:22 +0900

    인터셉터 개발

--- src/main/kotlin/com/kimauto/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/kimauto/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.kimauto.portfolio.presentation.interceptor
 
+import com.kimauto.portfolio.domain.entity.HttpInterface
+import com.kimauto.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
--- src/main/kotlin/com/kimauto/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/kimauto/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -2,7 +2,18 @@ package com.kimauto.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**") //모든 경로에 대해서 넣어주는 의미
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**","h2**",
+                "/favicon.ico", "/error")
+    }
 }
