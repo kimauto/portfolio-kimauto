799c680f7b4e25aeffa3b599a7280448b8a1a93c
kimauto <dkskdksk111@gmail.com>
2024-08-23 15:18:40 +0900

    DTO 개발 공통개발

--- src/main/kotlin/com/kimauto/portfolio/admin/data/ApiResponse.kt
+++ src/main/kotlin/com/kimauto/portfolio/admin/data/ApiResponse.kt
@@ -3,5 +3,19 @@ package com.kimauto.portfolio.admin.data
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
+//공통화 진행
 class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+
+    companion object {
+        fun successCreate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 저장되었습니다.")
+        }
+        fun successUpdate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 수정되었습니다.")
+        }
+        fun successDelete(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 삭제되었습니다.")
+        }
+
+    }
 }
