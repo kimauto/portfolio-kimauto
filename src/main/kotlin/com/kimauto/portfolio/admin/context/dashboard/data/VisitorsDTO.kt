fd65bdb945f83b2cf62c0fda4a4a5144f370dbe4
kimauto <dkskdksk111@gmail.com>
2024-09-08 17:29:20 +0900

    대시보드 개발 - 컨트롤러, 서비스, DTO

---
+++ src/main/kotlin/com/kimauto/portfolio/admin/context/dashboard/data/VisitorsDTO.kt
@@ -0,0 +1,12 @@
+package com.kimauto.portfolio.admin.context.dashboard.data
+
+import org.springframework.boot.ansi.AnsiBackground
+
+data class VisitorsDTO(
+    val name: String,
+    val count: Long,
+    val color: String,
+    val background: String
+) {
+
+}
